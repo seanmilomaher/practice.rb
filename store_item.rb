42d2946412ba271f9c3fbcfac351cb8bdeebf419
sean <maherseanmilo@gmail.com>
2021-01-06 14:22:35 -0600

    changes data to hash

--- store_item.rb
+++ store_item.rb
@@ -7,35 +7,12 @@
 # puts "The Defender is a driver with a speed of #{Defender[:speed]} and a glide of #{Defender[:glide]}. It's very stable with a turn of #{Defender[:turn]} and a fade of #{Defender[:fade]} so it'll try to fight out of the air very quickly."
 
 class Disc
-  def initialize(input_speed, input_glide, input_turn, input_fade)
-    @speed = input_speed
-    @glide = input_glide
-    @turn = input_turn
-    @fade = input_fade
-  end
-
-  def speed
-    @speed
-  end
-
-  def glide
-    @glide
-  end
-  
-  def turn
-    @turn
-  end
-
-  def fade
-    @fade
-  end
-
-  def turn=(input_glide)
-    @turn = input_glide
-  end
-
-  def fade(input_fade)
-    @fade = input_fade
+  attr_accessor :speed, :glide, :turn, :fade
+  def initialize(input_options)
+    @speed = input_options[:speed]
+    @glide = input_options[:glide]
+    @turn = input_options[:turn]
+    @fade = input_options[:fade]
   end
 
   def print_info
@@ -43,7 +20,7 @@ class Disc
   end
 end
 
-Defender = Disc.new(13, 5, 0, 3)
-Judge = Disc.new(2, 4, 0, 1)
-Truth = Disc.new(5, 5, -1, 1)
-Truth.print_info
+Defender = Disc.new(speed: 13, glide: 5, turn: 0, fade: 3)
+Judge = Disc.new(speed: 2, glide: 4, turn: 0, fade: 1)
+Truth = Disc.new(speed: 5, glide: 4, turn: -1, fade: 1)
+p Truth.speed
