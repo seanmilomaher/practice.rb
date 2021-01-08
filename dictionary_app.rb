59d7ec14f7cde59c4239050b60c1a3ca5b5fafeb
sean <maherseanmilo@gmail.com>
2021-01-08 15:15:34 -0600

    adds parsing for definition and example

---
+++ dictionary_app.rb
@@ -0,0 +1,10 @@
+require "http"
+
+puts "Enter a word, yo:"
+word = gets.chomp
+definition_response = HTTP.get("https://api.wordnik.com/v4/word.json/#{word}/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=7rzrusqms9ysbvd11ihq7idgk3slkzghxnqc6k6rmn3520mq9")
+
+example_response = HTTP.get("https://api.wordnik.com/v4/word.json/banana/examples?includeDuplicates=false&useCanonical=false&limit=5&api_key=7rzrusqms9ysbvd11ihq7idgk3slkzghxnqc6k6rmn3520mq9")
+
+p definition_response.parse[0]["text"]
+p example_response.parse["examples"][0]
